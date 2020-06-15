ea60618abb221b9da7ce29c0c2a3d6867c6ee5b2
Adrian Heaney <adrn@hotmail.co.uk>
2020-06-15 22:37:27 +0100

    Add redis and cache loaded data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
   def index
-    @users = []
+    @users = Rails.cache.fetch(cache_key)&.sort_by!(&:user_id)
   end
 
   def import
-    # @base_location = { latitude: import_params[:latitude], longitude: import_params[:longitude] }
-    @users = File.open(import_params[:user_batch]).map do |user|
+    parsed_file = File.open(import_params[:user_batch]).map do |user|
       User.new(JSON.parse(user).deep_symbolize_keys)
     end
-
-    @users.sort_by!(&:user_id)
+    Rails.cache.write(cache_key, parsed_file, expires_in: 2.hours)
 
     notice = 'loaded users'
-    render :index, notice: notice
-  # rescue Exception => e
-  #   notice = 'Oops, we weren\'t able to process that file, please check it is in the correct format and try again.' +
-  #            "\n" + e.message
-  #   redirect_to users_path, notice: notice
+    redirect_to users_path, notice: notice
+  rescue Exception => e
+    notice = "Oops, we weren't able to process that file, please check it is in the correct format and try again."
+    redirect_to users_path, notice: notice
   end
 
   private
+
+  def cache_key
+    session[:results_key] ||= SecureRandom.uuid
+  end
 
   def import_params
     params.permit(:latitude, :longitude, :user_batch)
