0b788fc2f26b978973c1d8034f2265e2b0f41210
Adrian Heaney <adrn@hotmail.co.uk>
2020-06-15 17:39:10 +0100

    parse file and calculate distances

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
-class User < ApplicationRecord
+class User
+  attr_accessor :latitude, :longitude, :name, :user_id
+
+  def initialize(name: nil, latitude: nil, longitude: nil, user_id: nil)
+    @name = name
+    raise 'Name cannot be blank' if @name.blank?
+
+    @latitude = BigDecimal(latitude)
+    raise 'Latitude must be provided and be a number' if @latitude.blank? || !@latitude.is_a?(Numeric)
+
+    @longitude = BigDecimal(longitude)
+    raise 'Longitude must be provided and be a number' if @longitude.blank? || !@longitude.is_a?(Numeric)
+
+    @user_id = user_id
+  end
+
+  def distance_to(latitude: 0, longitude: 0)
+    SphereSurfaceDistance::Calculator.surface_distance_on_earth(
+      { latitude: latitude, longitude: longitude },
+      { latitude: @latitude, longitude: @longitude }
+    )
+  end
 end
